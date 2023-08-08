bb2176a3ffb57b4b15138252d00967c2a90b984f
Juhász Szabolcs <juhaszszabolcs90@gmail.com>
2023-08-08 17:23:53 +0200

    Add basic layout for the webpage

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('layout.html')
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        port=5000
+    )
